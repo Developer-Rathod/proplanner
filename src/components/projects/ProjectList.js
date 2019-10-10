ab0abb9e0f60e1ba4b197e4e6396ef1f6c47a99f
Developer-Rathod <developer.rathod@gmail.com>
2019-10-10 15:34:44 +0300

    Project create

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,20 +1,9 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
-        <div className="section">
-                    <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                <span class="card-title">Card Title</span>
-                <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-                </div>
-                <div class="card-action">
-                <a href="#">This is a link</a>
-                <a href="#">This is a link</a>
-                </div>
-            </div>
-        </div>
+        <ProjectSummary />
     )
 }
 
