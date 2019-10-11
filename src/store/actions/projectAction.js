96339eea0f0feddc626585f612319345e41f5613
Developer-Rathod <developer.rathod@gmail.com>
2019-10-11 16:37:49 +0300

    dispatch action

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,7 +1,14 @@
 
 export const createProject = (project) => {
+    /* //what we usually do which just return actions
+        return{
+            type: 'CREATE_PROJECT',
+            project: project
+        } */
     return (dispatch, getState) => {
-        dispatch({ type: 'CREATE_PROJECT', project });
+        
+        //make async call to databse firestore
+        dispatch({ type: 'CREATE_PROJECT', project }); //posing the dispatch
     }
 };
  export const deleteProject = (id) =>{
